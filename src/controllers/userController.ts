9a6c4812bda20c4ed032d133444cea791a8c5c5d
Tayokanch <tayoquadri11@gmail.com>
2025-11-17 21:56:13 +0000

    simulate users for test

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,20 +1,21 @@
-import express from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-import { User } from '../DB/user';
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { User } from "../DB/user";
+import data from "../DB/data";
 
 dotenv.config();
 const SECRET = process.env.SECRET;
 if (!SECRET) {
-  throw new Error('Secret not found');
+  throw new Error("Secret not found");
 }
 const signUp = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, name, role } = req.body;
 
     if (!email || !password || !name) {
-      return res.status(400).json({ error: 'field missing' });
+      return res.status(400).json({ error: "field missing" });
     }
 
     const checkExistingUser = await User.findOne({ email });
@@ -31,7 +32,7 @@ const signUp = async (req: express.Request, res: express.Response) => {
     return res.status(200).json({ message: "You've successfully signed up" });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -39,60 +40,76 @@ const login = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.status(400).json({ error: 'One of the fields is missing' });
+      return res.status(400).json({ error: "One of the fields is missing" });
     }
 
     const foundUser = await User.findOne({ email });
     if (!foundUser) {
-      return res.status(400).json({ error: 'Inavlid email or password' });
+      return res.status(400).json({ error: "Inavlid email or password" });
     }
 
     const verifyPassword = await bcrypt.compare(password, foundUser.password);
     if (!verifyPassword) {
-      return res.status(400).json({ error: 'Email or password not correct' });
+      return res.status(400).json({ error: "Email or password not correct" });
     }
 
     const { _id, role, name } = foundUser;
     const payload = { id: _id.toString(), role };
-    const token = jwt.sign(payload, SECRET, { expiresIn: '15m' });
-    const refreshToken = jwt.sign(payload, SECRET, { expiresIn: '7d' });
+    const token = jwt.sign(payload, SECRET, { expiresIn: "15m" });
+    const refreshToken = jwt.sign(payload, SECRET, { expiresIn: "7d" });
 
- res.cookie('refreshToken', refreshToken, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === 'production', 
-  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', 
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-  path: '/api/refreshToken',
-});
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      path: "/api/refreshToken",
+    });
 
-
-    return res
-      .status(200)
-      .json({ login: true, role: role, id: _id.toString(), accessToken: token, username:name });
+    return res.status(200).json({
+      login: true,
+      role: role,
+      id: _id.toString(),
+      accessToken: token,
+      username: name,
+    });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 const logOut = (req: express.Request, res: express.Response) => {
   const token = req.cookies.token;
-  token ? console.log("Here's Token :", token) : console.log("I can't find Token")
+  token
+    ? console.log("Here's Token :", token)
+    : console.log("I can't find Token");
   try {
     if (!token)
       return res
         .status(401)
-        .json({ message: 'Token not found. Please log in' });
-    res.clearCookie('token', {
+        .json({ message: "Token not found. Please log in" });
+    res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === "production",
     });
 
     return res.status(204).end();
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ message: 'Internal server error' });
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
-export { signUp, login, logOut };
+const getUser = (req: express.Request, res: express.Response) => {
+  try {
+    if (data) {
+      return res.status(200).json({ users: data });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export { signUp, login, logOut, getUser };
