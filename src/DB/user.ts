ec6d912e602c0e5c7ce97fc5b5ff424f67e73b7b
tayo <you@example.com>
2025-11-22 18:04:42 +0000

    updated DB/user.ts

--- src/DB/user.ts
+++ src/DB/user.ts
@@ -1,28 +1,38 @@
-import mongoose, { Schema } from 'mongoose';
-import { UserI } from '../services.ts/interface';
+import mongoose, { Schema } from "mongoose";
+import { UserI } from "../services.ts/interface";
 
-const UserSchema: Schema<UserI> = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, select: true },
-    role: { 
-      type: String,
-      enum: ['user', 'vogueadmin'],
-      default: 'user',
 
-    }
+    email: { type: String, required: true, unique: true },
+
+    password: { type: String, required: true, select: true },
+
+    role: {
+      type: String,
+      enum: ["user", "vogueadmin"],
+      default: "user",
+    },
   },
   {
     timestamps: true,
-    toJSON: {
-      transform: (doc, ret) => {
-        ret._id = ret._id.toString();
-        delete ret.__v;
-      },
-    },
+    versionKey: false,
   }
 );
 
 
-export const User = mongoose.model<UserI>("User", UserSchema)
+UserSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+
+UserSchema.set("toJSON", {
+  virtuals: true,
+  transform: function (doc, ret: any) {
+    delete ret._id; // hide Mongo's _id
+  },
+});
+
+export const User = mongoose.model<UserI>("User", UserSchema);
+
