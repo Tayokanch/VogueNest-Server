59eeb95f3ca49959c5cde18befee9c08413d3f71
Tayokanch <tayoquadri11@gmail.com>
2025-11-23 19:10:25 +0000

    added client domain

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['https://voguenestt.netlify.app', 'http://localhost:5173'],
+  origin: ['https://vogueshopping.tayolabs.dev/', 'http://localhost:5173'],
   credentials: true,
 }));
 
